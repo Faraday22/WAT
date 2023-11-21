b6c094a7f35d2c3cfd099f421e00e34b0023efcc
Faraday <115096700+Faraday22@users.noreply.github.com>
2023-11-21 16:22:13 +0000

    Install instructions + text colors

--- README.md
+++ README.md
@@ -4,3 +4,9 @@
 ——————————————————————————  
 A wallpaper applicance tool for Sway WM
 to help the process of switching wallpapers easier.
+
+To install please use:  
+    ```chmod +x wat.lua```
+    (this will need sudo privleges)
+    ```mv /usr/local/bin/wat```
+    then you will be able to run 'wat' in terminal
--- wat.lua
+++ wat.lua
@@ -1,49 +1,53 @@
+#!/bin/env lua
+
+-- Define ANSI escape codes for colors
+local colorGold = "\27[33m"
+local colorReset = "\27[0m"
+
 --Vaiables & Functions
 local function title()
   os.execute("clear")
-  print("┓ ┏┏┓┏┳┓")
-  print("┃┃┃┣┫ ┃")
-  print("┗┻┛┛┗ ┻")
-  print("---------\n")
-  print("WAT the wallpaper applicance tool\n")
+  print(colorGold .."┓ ┏┏┓┏┳┓" ..colorReset)
+  print(colorGold .."┃┃┃┣┫ ┃" ..colorReset)
+  print(colorGold .."┗┻┛┛┗ ┻" ..colorReset)
+  print(colorGold .. "--------------\n" ..colorReset)
+  print(colorGold .."WAT the wallpaper applicance tool\n" ..colorReset)
 end
 
 
 local function startScreen()
   os.execute("clear")
   title()
-  os.execute("sleep 1")
+  os.execute("sleep 0.5")
 end
 
 
 --start code
 startScreen()
 
-print("If stored in home directory enter 'Y/y' if not 'N/n'\n")
+print(colorGold .."If stored in home directory enter 'Y/y' if not 'N/n'\n" ..colorReset)
 local homeDir = io.read()
 
 if homeDir == "Y" or homeDir == "y" then
   os.execute("clear")
-  print("Enter the directory name")
+  print(colorGold.."Enter the directory name" ..colorReset)
   local dirPath = io.read()
   os.execute("clear")
 
   os.execute("ls ~/" .. dirPath)
-  print("Choose the file")
+  print(colorGold .."Choose the file" ..colorReset)
   local imageName = io.read()
-
   os.execute("swaymsg output '*' bg ~/" ..dirPath.."/".. imageName.. " fill")
 end
 
 if homeDir == "N" or homeDir == "n" then
   os.execute("clear")
-  print("Enter where directory is")
+  print(colorGold.."Enter where directory is"..colorReset)
   local dirPath = io.read()
   os.execute("clear")
 
   os.execute("ls " .. dirPath)
-  print("Choose the file")
+  print(colorGold.."Choose the file"..colorReset)
   local imagePath = io.read()
-
   os.execute("swaymsg output '*' bg " ..dirPath.. "/".. imagePath .. " fill")
 end
